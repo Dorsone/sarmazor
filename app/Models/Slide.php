c28652434b25b49fae04e9d094ff498f2207cb74
Dorsone <jasur.coder@gmail.com>
2024-05-12 15:40:25 +0500

    wip

--- app/Models/Slide.php
+++ app/Models/Slide.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
+use Orchid\Attachment\Attachable;
+use Orchid\Filters\Filterable;
+use Orchid\Screen\AsSource;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Translatable\HasTranslations;
@@ -23,6 +26,9 @@ class Slide extends Model implements HasMedia
     use HasFactory;
     use HasTranslations;
     use InteractsWithMedia;
+    use AsSource;
+    use Filterable;
+    use Attachable;
 
     public array $translatable = [
         'title',
